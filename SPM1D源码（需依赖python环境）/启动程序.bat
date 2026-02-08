18ca3f91ef55d1da20e5dadf06b3e45106edd0f5
shanshanfagu <134119443+shanshanfagu@users.noreply.github.com>
2026-02-08 12:15:01 +0800

    Delete SPM1D源码（需依赖python环境）/启动程序.bat

--- SPM1D源码（需依赖python环境）/启动程序.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ============================================
-echo      SPM1D 分析软件
-echo ============================================
-echo.
-"D:\Annoconda\python.exe" SPM1D.py
-pause
